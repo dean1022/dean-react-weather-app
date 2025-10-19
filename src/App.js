64f25c70b4e814b5de5310624614b52d64ecaed6
lasini1022 <lasinikurukulasooriya@gmail.com>
2025-10-19 13:50:46 -0400

    made footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://deanlucianvoss.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dean Voss
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/dean1022/dean-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
