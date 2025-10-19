05755d156f77abb895e52945ccedba9678466d11
lasini1022 <lasinikurukulasooriya@gmail.com>
2025-10-19 17:18:28 -0400

    added axios and api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="container Weather">
-      <div className="row">
-        <form>
-          <div className="input-group">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-            />
-            <button type="submit" className="btn btn-primary">
-              Search
-            </button>
+  let [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response);
+    setWeatherData({
+      city: response.data.city,
+      country: response.data.country,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      icon_url: response.data.condition.icon_url,
+      feels_like: response.data.temperature.feels_like,
+      humidity: response.data.temperature.humidity,
+      pressure: response.data.temperature.pressure,
+      wind: response.data.wind.speed,
+      ready: true,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="container Weather">
+        <div className="row">
+          <form>
+            <div className="input-group">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+              />
+              <button type="submit" className="btn btn-primary">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+        <h4 className="row city">
+          {weatherData.city}
+          {", "}
+          {weatherData.country}
+        </h4>
+        <div className="row">
+          <div className="col-1 main-img">
+            <img src={weatherData.icon_url} alt={weatherData.description} />
           </div>
-        </form>
-      </div>
-      <h4 className="row city">Toronto, ON, Canada</h4>
-      <div className="row">
-        <div className="col-1 main-img">
-          <img
-            src="https://www.gstatic.com/weather/conditions2023/2023.2/svg/cloudy_light.svg"
-            alt="Cloudy"
-          />
-        </div>
-        <div className="col-1 deg">
-          <strong className="today-deg">19</strong>°C
-        </div>
-        <div className="col-4 details">
-          <ul>
-            <li>Precipitation: 0%</li>
-            <li>Humidity: 71%</li>
-            <li>Wind: 27 km/h</li>
-          </ul>
-        </div>
-        <div className="col-4 description">
-          <p className="title">Weather</p>
-          <p className="date">Sunday 3:00 PM</p>
-          <p className="today-weather-desc">Cloudy</p>
+          <div className="col-1 deg">
+            <strong className="today-deg">
+              {Math.round(weatherData.temperature)}
+            </strong>
+            °C
+          </div>
+          <div className="col-4 details">
+            <ul>
+              <li>Feels Like: {Math.round(weatherData.feels_like)}°C</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+              <li>Pressure: {weatherData.pressure}</li>
+            </ul>
+          </div>
+          <div className="col-4 description">
+            <p className="title">Weather</p>
+            <p className="date">Sunday 3:00 PM</p>
+            <p className="today-weather-desc">{weatherData.description}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "14b3e71od6df40977d863a00tdaeb6ef";
+    let city = "Toronto";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
