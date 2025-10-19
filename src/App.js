cffd97e206c8fa6116001143d5282f9b40b1c5a5
lasini1022 <lasinikurukulasooriya@gmail.com>
2025-10-19 14:08:12 -0400

    added bootstrap and changed text decoration for footer

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://deanlucianvoss.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Dean Voss
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/dean1022/dean-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://deanlucianvoss.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dean Voss
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/dean1022/dean-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
